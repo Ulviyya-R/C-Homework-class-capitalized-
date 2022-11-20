16831b17398b96b72b53366ccc831f665cde1d44
Ulviyya M. Rustamova <ulviyyamr@code.local>
2022-11-20 18:47:35 +0400

    This is homeworks

--- Program.cs
+++ Program.cs
@@ -6,12 +6,51 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Student1 student = new Student1("Ulviyya", "Rustamova", "P230", 70, false);
-            Console.WriteLine(student.Name);
+            #region Task 2
+            //Student1 student = new Student1("Ulviyya", "Rustamova", "P230", 85,true);
+            //Student1 student2 = new Student1("Sudaba", "Alihuseynli", "P330", 63, false);
+            //Console.WriteLine(student2.Name);
 
-            student.Graduated();
-            student.Points();
+            //student.Graduated();
+            //student.Points();
+
+            //student2.Graduated();
+            //student2.Points();
 
         }
     }
+    #endregion
+
+
+    #region Task1 
+    //string sentence = Console.ReadLine();
+    //string sentence2 = sentence.ToUpper();
+
+    //SentenceUpper(sentence2);
+
+    //static void SentenceUpper(string sentence)
+
+    //{
+    //    for (int i = 0; i < sentence.Length; i++)
+    //    {
+    //        if (i == 0)
+    //        {
+    //            Console.Write(sentence[i]);
+    //            sentence = sentence.ToLower();
+    //            i++;
+
+    //        }
+    //        if (sentence[i] == ' ')
+    //        {
+    //            sentence = sentence.ToUpper();
+    //            Console.Write(sentence[i]);
+    //            i++;
+    //        }
+
+    //        Console.Write(sentence[i]);
+    //        sentence = sentence.ToLower();
+
+    //    }
+    #endregion
 }
+    
