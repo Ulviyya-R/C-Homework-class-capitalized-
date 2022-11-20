d71e53a1b4cfcca60818cb8466b1b7af4baa1954
Ulviyya M. Rustamova <ulviyyamr@code.local>
2022-11-20 17:53:39 +0400

    This is about class code

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student1 student = new Student1("Ulviyya", "Rustamova", "P230", 70, false);
+            Console.WriteLine(student.Name);
+
+            student.Graduated();
+            student.Points();
+
+        }
+    }
+}
---
+++ Students.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Student1
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Point;
+        public bool IsGraduated;
+
+        public Student1(string name, string surname, string group, int point, bool isgraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            IsGraduated = isgraduated;
+
+        }
+
+        public void Graduated()
+        {
+            if (IsGraduated)
+            {
+                Console.WriteLine("This student has already graduated");
+            }
+            else
+                Console.WriteLine("This student will continue his studies");
+        }
+
+
+        public void Points()
+        {
+            if (Point > 80)
+            {
+                Console.WriteLine(" You are registered for the next exam ");
+            }
+            else
+            {
+                Console.WriteLine(" Sorry that you are not admitted to the next exam ");
+            }
+        }
+
+    }
+}
+
